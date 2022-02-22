d85fd17637b286951437a8af7fdbc3d7c6ba0e8f
Warxcell <warxcell@gmail.com>
2022-02-22 22:45:43 +0200

    Fix

--- src/Router.php
+++ src/Router.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 $now = new DateTimeImmutable();
 $line = sprintf('%s: %s %s', $now->format('d.m.Y H:i:s.u'), $_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
 
-fwrite(STDOUT, $line . PHP_EOL);
+file_put_contents('php://stdout', $line . PHP_EOL, FILE_APPEND);
 
 $documentRoot = $_SERVER['DOCUMENT_ROOT'];
 $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
